8368b51df0300e07c8abfe4d69a9179a16315445
huyletran <129037971+huyletran@users.noreply.github.com>
2023-07-05 11:32:18 +0700

    Update encoder.py

--- comer/model/encoder.py
+++ comer/model/encoder.py
@@ -11,142 +11,118 @@ from torch import FloatTensor, LongTensor
 from .pos_enc import ImgPosEnc
 
 
-# DenseNet-B
-class _Bottleneck(nn.Module):
-    def __init__(self, n_channels: int, growth_rate: int, use_dropout: bool):
-        super(_Bottleneck, self).__init__()
-        interChannels = 4 * growth_rate
-        self.bn1 = nn.BatchNorm2d(interChannels)
-        self.conv1 = nn.Conv2d(n_channels, interChannels, kernel_size=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(growth_rate)
-        self.conv2 = nn.Conv2d(
-            interChannels, growth_rate, kernel_size=3, padding=1, bias=False
+class ConvNextStem(nn.Sequential):
+    def __init__(self, in_features: int, out_features: int):
+        super().__init__(
+            nn.Conv2d(in_features, out_features, kernel_size=4, stride=4),
+            nn.BatchNorm2d(out_features)
         )
-        self.use_dropout = use_dropout
-        self.dropout = nn.Dropout(p=0.2)
 
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
-        if self.use_dropout:
-            out = self.dropout(out)
-        out = F.relu(self.bn2(self.conv2(out)), inplace=True)
-        if self.use_dropout:
-            out = self.dropout(out)
-        out = torch.cat((x, out), 1)
-        return out
-
-
-# single layer
-class _SingleLayer(nn.Module):
-    def __init__(self, n_channels: int, growth_rate: int, use_dropout: bool):
-        super(_SingleLayer, self).__init__()
-        self.bn1 = nn.BatchNorm2d(n_channels)
-        self.conv1 = nn.Conv2d(
-            n_channels, growth_rate, kernel_size=3, padding=1, bias=False
+class ConvNexStage(nn.Sequential):
+    def __init__(
+        self, in_features: int, out_features: int, depth: int, **kwargs
+    ):
+        super().__init__(
+            # add the downsampler
+            nn.Sequential(
+                nn.GroupNorm(num_groups=1, num_channels=in_features),
+                nn.Conv2d(in_features, out_features, kernel_size=2, stride=2)
+            ),
+            *[
+                BottleNeckBlock(out_features, out_features, **kwargs)
+                for _ in range(depth)
+            ],
         )
-        self.use_dropout = use_dropout
-        self.dropout = nn.Dropout(p=0.2)
+from torchvision.ops import StochasticDepth
+
+class LayerScaler(nn.Module):
+    def __init__(self, init_value: float, dimensions: int):
+        super().__init__()
+        self.gamma = nn.Parameter(init_value * torch.ones((dimensions)),
+                                    requires_grad=True)
 
     def forward(self, x):
-        out = self.conv1(F.relu(x, inplace=True))
-        if self.use_dropout:
-            out = self.dropout(out)
-        out = torch.cat((x, out), 1)
-        return out
+        return self.gamma[None,...,None,None] * x
 
-
-# transition layer
-class _Transition(nn.Module):
-    def __init__(self, n_channels: int, n_out_channels: int, use_dropout: bool):
-        super(_Transition, self).__init__()
-        self.bn1 = nn.BatchNorm2d(n_out_channels)
-        self.conv1 = nn.Conv2d(n_channels, n_out_channels, kernel_size=1, bias=False)
-        self.use_dropout = use_dropout
-        self.dropout = nn.Dropout(p=0.2)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
-        if self.use_dropout:
-            out = self.dropout(out)
-        out = F.avg_pool2d(out, 2, ceil_mode=True)
-        return out
-
-
-class DenseNet(nn.Module):
+class BottleNeckBlock(nn.Module):
     def __init__(
         self,
-        growth_rate: int,
-        num_layers: int,
-        reduction: float = 0.5,
-        bottleneck: bool = True,
-        use_dropout: bool = True,
+        in_features: int,
+        out_features: int,
+        expansion: int = 4,
+        drop_p: float = .0,
+        layer_scaler_init_value: float = 1e-6,
     ):
-        super(DenseNet, self).__init__()
-        n_dense_blocks = num_layers
-        n_channels = 2 * growth_rate
-        self.conv1 = nn.Conv2d(
-            1, n_channels, kernel_size=7, padding=3, stride=2, bias=False
+        super().__init__()
+        expanded_features = out_features * expansion
+        self.block = nn.Sequential(
+            # narrow -> wide (with depth-wise and bigger kernel)
+            nn.Conv2d(
+                in_features, in_features, kernel_size=7, padding=3, bias=False, groups=in_features
+            ),
+            # GroupNorm with num_groups=1 is the same as LayerNorm but works for 2D data
+            nn.GroupNorm(num_groups=1, num_channels=in_features),
+            # wide -> wide
+            nn.Conv2d(in_features, expanded_features, kernel_size=1),
+            nn.GELU(),
+            # wide -> narrow
+            nn.Conv2d(expanded_features, out_features, kernel_size=1),
         )
-        self.norm1 = nn.BatchNorm2d(n_channels)
-        self.dense1 = self._make_dense(
-            n_channels, growth_rate, n_dense_blocks, bottleneck, use_dropout
+        self.layer_scaler = LayerScaler(layer_scaler_init_value, out_features)
+        self.drop_path = StochasticDepth(drop_p, mode="batch")
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        res = x
+        x = self.block(x)
+        x = self.layer_scaler(x)
+        x = self.drop_path(x)
+        x += res
+        return x
+
+class ConvNextEncoder(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        stem_features: int,
+        depths: List[int],
+        widths: List[int],
+        drop_p: float = .0,
+    ):
+        super().__init__()
+        self.stem = ConvNextStem(in_channels, stem_features)
+
+        in_out_widths = list(zip(widths, widths[1:]))
+        # create drop paths probabilities (one for each stage)
+        drop_probs = [x.item() for x in torch.linspace(0, drop_p, sum(depths))]
+
+        self.stages = nn.ModuleList(
+            [
+                ConvNexStage(stem_features, widths[0], depths[0], drop_p=drop_probs[0]),
+                *[
+                    ConvNexStage(in_features, out_features, depth, drop_p=drop_p)
+                    for (in_features, out_features), depth, drop_p in zip(
+                        in_out_widths, depths[1:], drop_probs[1:]
+                    )
+                ],
+            ]
         )
-        n_channels += n_dense_blocks * growth_rate
-        n_out_channels = int(math.floor(n_channels * reduction))
-        self.trans1 = _Transition(n_channels, n_out_channels, use_dropout)
 
-        n_channels = n_out_channels
-        self.dense2 = self._make_dense(
-            n_channels, growth_rate, n_dense_blocks, bottleneck, use_dropout
-        )
-        n_channels += n_dense_blocks * growth_rate
-        n_out_channels = int(math.floor(n_channels * reduction))
-        self.trans2 = _Transition(n_channels, n_out_channels, use_dropout)
 
-        n_channels = n_out_channels
-        self.dense3 = self._make_dense(
-            n_channels, growth_rate, n_dense_blocks, bottleneck, use_dropout
-        )
+    def forward(self, x,x_mask):
+        x = self.stem(x)
 
-        self.out_channels = n_channels + n_dense_blocks * growth_rate
-        self.post_norm = nn.BatchNorm2d(self.out_channels)
-
-    @staticmethod
-    def _make_dense(n_channels, growth_rate, n_dense_blocks, bottleneck, use_dropout):
-        layers = []
-        for _ in range(int(n_dense_blocks)):
-            if bottleneck:
-                layers.append(_Bottleneck(n_channels, growth_rate, use_dropout))
-            else:
-                layers.append(_SingleLayer(n_channels, growth_rate, use_dropout))
-            n_channels += growth_rate
-        return nn.Sequential(*layers)
-
-    def forward(self, x, x_mask):
-        out = self.conv1(x)
-        out = self.norm1(out)
-        out_mask = x_mask[:, 0::2, 0::2]
-        out = F.relu(out, inplace=True)
-        out = F.max_pool2d(out, 2, ceil_mode=True)
-        out_mask = out_mask[:, 0::2, 0::2]
-        out = self.dense1(out)
-        out = self.trans1(out)
-        out_mask = out_mask[:, 0::2, 0::2]
-        out = self.dense2(out)
-        out = self.trans2(out)
-        out_mask = out_mask[:, 0::2, 0::2]
-        out = self.dense3(out)
-        out = self.post_norm(out)
-        return out, out_mask
-
+        for stage in self.stages:
+            x = stage(x)
+        return x ,x_mask
 
 class Encoder(pl.LightningModule):
     def __init__(self, d_model: int, growth_rate: int, num_layers: int):
         super().__init__()
 
-        self.model = DenseNet(growth_rate=growth_rate, num_layers=num_layers)
+        self.model = ConvNextEncoder(in_channels=3, stem_features=64, depths=[3,4,6,4], widths=[256, 512])
 
-        self.feature_proj = nn.Conv2d(self.model.out_channels, d_model, kernel_size=1)
+        self.feature_proj = nn.Conv2d(512, d_model, kernel_size=1)
 
         self.pos_enc_2d = ImgPosEnc(d_model, normalize=True)
 
