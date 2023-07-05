071e3483cf72560103dd202ad8996fb5fd15c01e
huyletran <129037971+huyletran@users.noreply.github.com>
2023-07-05 14:38:18 +0700

    Update encoder.py

--- comer/model/encoder.py
+++ comer/model/encoder.py
@@ -927,7 +927,7 @@ class EfficientNet(nn.Module):
         """
         # Convolution layers
         x = self.extract_features(inputs)
-        out_mask = x_mask[:,0:4,0:4]
+        out_mask = x_mask
         return x,out_mask
 
     @classmethod
