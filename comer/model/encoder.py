f30b858f5b0ab083453f126b7f534b41431083cd
huyletran <129037971+huyletran@users.noreply.github.com>
2023-07-06 17:26:01 +0700

    Update encoder.py

--- comer/model/encoder.py
+++ comer/model/encoder.py
@@ -15,129 +15,154 @@ import torch.nn.functional as F
 from timm.models.layers import trunc_normal_, DropPath
 from timm.models.registry import register_model
 
-class Block(nn.Module):
-    r""" ConvNeXt Block. There are two equivalent implementations:
-    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
-    (2) DwConv -> Permute to (N, H, W, C); LayerNorm (channels_last) -> Linear -> GELU -> Linear; Permute back
-    We use (2) as we find it slightly faster in PyTorch
+class ConvNextStem(nn.Sequential):
+    def __init__(self, in_features: int, out_features: int):
+        super().__init__(
+            nn.Conv2d(in_features, out_features, kernel_size=4, stride=4),
+            nn.BatchNorm2d(out_features)
+        )
 
-    Args:
-        dim (int): Number of input channels.
-        drop_path (float): Stochastic depth rate. Default: 0.0
-        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
-    """
-    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+class ConvNexStage(nn.Sequential):
+    def __init__(
+        self, in_features: int, out_features: int, depth: int, **kwargs
+    ):
+        super().__init__(
+            # add the downsampler
+            nn.Sequential(
+                nn.GroupNorm(num_groups=1, num_channels=in_features),
+                nn.Conv2d(in_features, out_features, kernel_size=2, stride=2)
+            ),
+            *[
+                BottleNeckBlock(out_features, out_features, **kwargs)
+                for _ in range(depth)
+            ],
+        )
+from timm.models.layers import trunc_normal_, DropPath
+
+class LayerScaler(nn.Module):
+    def __init__(self, init_value: float, dimensions: int):
         super().__init__()
-        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim) # depthwise conv
-        self.norm = LayerNorm(dim, eps=1e-6)
-        self.pwconv1 = nn.Linear(dim, 4 * dim) # pointwise/1x1 convs, implemented with linear layers
-        self.act = nn.GELU()
-        self.pwconv2 = nn.Linear(4 * dim, dim)
-        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
-                                    requires_grad=True) if layer_scale_init_value > 0 else None
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.gamma = nn.Parameter(init_value * torch.ones((dimensions)),
+                                    requires_grad=True)
 
     def forward(self, x):
-        input = x
-        x = self.dwconv(x)
-        x = x.permute(0, 2, 3, 1) # (N, C, H, W) -> (N, H, W, C)
-        x = self.norm(x)
-        x = self.pwconv1(x)
-        x = self.act(x)
-        x = self.pwconv2(x)
-        if self.gamma is not None:
-            x = self.gamma * x
-        x = x.permute(0, 3, 1, 2) # (N, H, W, C) -> (N, C, H, W)
+        return self.gamma[None,...,None,None] * x
 
-        x = input + self.drop_path(x)
+class BottleNeckBlock(nn.Module):
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        expansion: int = 4,
+        drop_p: float = .0,
+        layer_scaler_init_value: float = 1e-6,
+    ):
+        super().__init__()
+        expanded_features = out_features * expansion
+        self.block = nn.Sequential(
+            # narrow -> wide (with depth-wise and bigger kernel)
+            nn.Conv2d(
+                in_features, in_features, kernel_size=7, padding=3, bias=False, groups=in_features
+            ),
+            # GroupNorm with num_groups=1 is the same as LayerNorm but works for 2D data
+            nn.GroupNorm(num_groups=1, num_channels=in_features),
+            # wide -> wide
+            nn.Conv2d(in_features, expanded_features, kernel_size=1),
+            nn.GELU(),
+            # wide -> narrow
+            nn.Conv2d(expanded_features, out_features, kernel_size=1),
+        )
+        self.layer_scaler = LayerScaler(layer_scaler_init_value, out_features)
+        self.drop_path = DropPath(drop_p) if drop_p > 0. else nn.Identity()
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        res = x
+        x = self.block(x)
+        x = self.layer_scaler(x)
+        x = self.drop_path(x)
+        x += res
         return x
 
-class ConvNeXt(nn.Module):
-    r""" ConvNeXt
-        A PyTorch impl of : `A ConvNet for the 2020s`  -
-          https://arxiv.org/pdf/2201.03545.pdf
-
-    Args:
-        in_chans (int): Number of input image channels. Default: 3
-        num_classes (int): Number of classes for classification head. Default: 1000
-        depths (tuple(int)): Number of blocks at each stage. Default: [3, 3, 9, 3]
-        dims (int): Feature dimension at each stage. Default: [96, 192, 384, 768]
-        drop_path_rate (float): Stochastic depth rate. Default: 0.
-        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
-        head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
-    """
-    def __init__(self, in_chans=1, num_classes=1000,
-                 depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], drop_path_rate=0.,
-                 layer_scale_init_value=1e-6, head_init_scale=1.,
-                 ):
+class ConvNextEncoder(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        stem_features: int,
+        depths: List[int],
+        widths: List[int],
+        drop_p: float = .0,
+    ):
         super().__init__()
+        self.stem = ConvNextStem(in_channels, stem_features)
 
-        self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
-        stem = nn.Sequential(
-            nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
-            LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
+        in_out_widths = list(zip(widths, widths[1:]))
+        # create drop paths probabilities (one for each stage)
+        drop_probs = [x.item() for x in torch.linspace(0, drop_p, sum(depths))]
+
+        self.stages = nn.ModuleList(
+            [
+                ConvNexStage(stem_features, widths[0], depths[0], drop_p=drop_probs[0]),
+                *[
+                    ConvNexStage(in_features, out_features, depth, drop_p=drop_p)
+                    for (in_features, out_features), depth, drop_p in zip(
+                        in_out_widths, depths[1:], drop_probs[1:]
+                    )
+                ],
+            ]
         )
-        self.downsample_layers.append(stem)
-        for i in range(3):
-            downsample_layer = nn.Sequential(
-                    LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
-                    nn.Conv2d(dims[i], dims[i+1], kernel_size=2, stride=2),
-            )
-            self.downsample_layers.append(downsample_layer)
-
-        self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks
-        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
-        cur = 0
-        for i in range(4):
-            stage = nn.Sequential(
-                *[Block(dim=dims[i], drop_path=dp_rates[cur + j],
-                layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
-            )
-            self.stages.append(stage)
-            cur += depths[i]
-
-        self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
 
 
-    def forward(self, x):
-        for i in range(4):
-            x = self.downsample_layers[i](x)
-            x = self.stages[i](x)
-        return self.norm(x.mean([-2, -1])) # global average pooling, (N, C, H, W) -> (N, C)
+    def forward(self, x,x_mask):
+        x = self.stem(x)
 
+        for stage in self.stages:
+            x = stage(x)
+            x_mask = x_mask[:, 0::2, 0::2]
+        return x ,x_mask
 
-class LayerNorm(nn.Module):
-    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first.
-    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with
-    shape (batch_size, height, width, channels) while channels_first corresponds to inputs
-    with shape (batch_size, channels, height, width).
-    """
-    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+class Encoder(pl.LightningModule):
+    def __init__(self, d_model: int, growth_rate: int, num_layers: int):
         super().__init__()
-        self.weight = nn.Parameter(torch.ones(normalized_shape))
-        self.bias = nn.Parameter(torch.zeros(normalized_shape))
-        self.eps = eps
-        self.data_format = data_format
-        if self.data_format not in ["channels_last", "channels_first"]:
-            raise NotImplementedError
-        self.normalized_shape = (normalized_shape, )
 
-    def forward(self, x):
-        if self.data_format == "channels_last":
-            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
-        elif self.data_format == "channels_first":
-            u = x.mean(1, keepdim=True)
-            s = (x - u).pow(2).mean(1, keepdim=True)
-            x = (x - u) / torch.sqrt(s + self.eps)
-            x = self.weight[:, None, None] * x + self.bias[:, None, None]
-            return x
+        self.model = ConvNextEncoder(in_channels=1, stem_features=64, depths=[3,4,6,4], widths=[256, 512])
 
+        self.feature_proj = nn.Conv2d(512, d_model, kernel_size=1)
 
+        self.pos_enc_2d = ImgPosEnc(d_model, normalize=True)
 
-@register_model
-def convnext_tiny(pretrained=False,in_22k=False, **kwargs):
-    model = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
-    return model
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(
+        self, img: FloatTensor, img_mask: LongTensor
+    ) -> Tuple[FloatTensor, LongTensor]:
+        """encode image to feature
+
+        Parameters
+        ----------
+        img : FloatTensor
+            [b, 1, h', w']
+        img_mask: LongTensor
+            [b, h', w']
+
+        Returns
+        -------
+        Tuple[FloatTensor, LongTensor]
+            [b, h, w, d], [b, h, w]
+        """
+        # extract feature
+        feature, mask = self.model(img, img_mask)
+        feature = self.feature_proj(feature)
+
+        # proj
+        feature = rearrange(feature, "b d h w -> b h w d")
+
+        # positional encoding
+        feature = self.pos_enc_2d(feature, mask)
+        feature = self.norm(feature)
+
+        # flat to 1-D
+        return feature, mask
     
 class Encoder(pl.LightningModule):
     def __init__(self, d_model: int, growth_rate: int, num_layers: int):
